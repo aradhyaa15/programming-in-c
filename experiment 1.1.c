5f502cdcfc2c7041516cc23f3212aaa7a44fadff
aradhyaa15 <aradhyaanand33@gmail.com>
2025-09-01 00:04:15 +0530

    Add files via upload

---
+++ experiment 1.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+printf("Name – Aradhya Anand\nSAP ID - 590028564\nCourse - BCA\nBatch - B6");
+printf("\n--------------------------------------------");
+printf("\nHello World");
+printf("\n--------------------------------------------");
+getch();
+return 0;
+}
---
+++ experiment 1.2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+
+ printf("Name - Aradhya Anand\n");
+ printf("SAP ID - 590028564\n");
+ printf("Course - BCA\n");
+ printf("Batch - B6\n");
+ printf("--------------------------------------------\n");
+
+ printf("Anshika home, lower bidholi\n");
+ printf("City: bidholi\n");
+ printf("State: uttrakhand\n");
+ printf("Pincode: 226017\n");
+ printf("--------------------------------------------\n");
+ printf("Press Enter to exit...");
+ getchar();
+ return 0;
+}
